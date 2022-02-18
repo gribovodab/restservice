552a4956a9663fb0f541345d96522665df30e285
gribovodab <98866263+gribovodab@users.noreply.github.com>
2022-02-18 07:00:28 +0300

    Update IRestServiceImpl.cs

--- RestService/IRestServiceImpl.cs
+++ RestService/IRestServiceImpl.cs
@@ -1,4 +1,6 @@
-﻿using System.ServiceModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.ServiceModel;
 using System.ServiceModel.Web;
 
 namespace RestService
@@ -21,4 +23,33 @@ namespace RestService
             UriTemplate = "json/{id}")]
         string JSONData(string id);
     }
+
+    public class ChangeProduct : INotifyPropertyChanged
+    {
+        private string imageFullPath;
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, e);
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        }
+        public string ImageFullPath
+        {
+            get { return imageFullPath; }
+            set
+            {
+                if (value != imageFullPath)
+                {
+                    imageFullPath = value;
+                    OnPropertyChanged("ImageFullPath");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
 }
